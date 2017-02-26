234503f02a98068a5c7b3b3c42831cad219b6938
Ponte Ineptique <Ponte Ineptique>
2017-02-26 11:41:51 +0100

    Fin session 1

---
+++ session-1/slide-29/script.js
@@ -0,0 +1,12 @@
+// Bug dans Firebug ?
+
+console.log(document.getElementById("header"));
+console.log(document.getElementById("article"));
+console.log(document.getElementById("articletitle"));
+
+console.log(document.getElementsByClassName("container"));
+console.log(document.getElementsByTagName("li"));
+
+console.log(document.getElementById("article").getElementsByTagName("p"));
+
+console.log("--");
